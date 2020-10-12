721fc5682edba750d356f973787f1ec2eec0900c
Sanjay <54913971+San1ay@users.noreply.github.com>
2020-10-12 17:01:49 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Automated Zoom Meetings
+
+This script can be used to join a scheduled Zoom meeting automacally. It's for lazy People like me. You can use it in Cloud like AWS or Google Cloud to get Free Attendence in case it's mandatory without attending.
+
+## Requirements
+
+- Installed [Python3](https://www.python.org/downloads/) above 3.5
+- Installed pyautogui package
+- Signed In [Zoom](https://zoom.us/download) App
+
+## Setting up some Extra configurations :
+
+### Make sure the Zoom settings are as follows
+
+- Mute Audio and Select join with Audio.
+- Turn off Video.
+
+#
+
+# How to run the program :
+
+### Clone this repository
+
+- Configure `main.py` as your required scheldule
+
+- Run `main.py`
+
+  - Make Sure `joinButton.png` is in same Directory
+
+- Run using `pythonw main.py` instead of `python main.py` to run script in background
+
+- Increase the time.sleep value as required if your PC or Internet speed is slow
+
+_*Drop a ⭐ Lazy Peoples*_
---
+++ main.py
@@ -0,0 +1,102 @@
+# Libraries imported
+import pyautogui 
+import datetime
+import os
+import time
+
+#Edit this file as Required
+def zoomClass(meetingID,meetingPassword,meetingTime):
+
+    # Edit Here
+    # enter your path to Zoom.exe
+    pathToZoom=r'C:\Users\Sanjay\AppData\Roaming\Zoom\bin\Zoom.exe'
+    os.system(pathToZoom)
+
+    # Adjust sleep time if your PC is slow
+    time.sleep(5)
+
+    x,y = pyautogui.locateCenterOnScreen('./joinButton.png')
+    pyautogui.click(x,y)
+
+    # Adjust Sleep time if Your Network speed is slow
+    pyautogui.press('enter',interval=5)
+    pyautogui.write(meetingID)
+    pyautogui.press('enter',interval=5)
+    pyautogui.write(meetingPassword)
+    pyautogui.press('enter',interval = 5)
+
+    print("Meeting started for %s minutes"%meetingTime)
+
+    #Total time of zoom session
+    time.sleep(meetingTime*60) 
+
+    # closing Zoom
+    os.system("TASKKILL /F /IM Zoom.exe")
+    return
+
+
+
+def getID(sub):
+    # Edit Here 
+    #Sub=[Meeting ID,Password]
+    Subs={"DS":["987654321","123456"],
+    "CO":["987654321","123456"],
+    "NMPS":["987654321","123456"],
+    "OS":["987654321","123456"],
+    "DMS":["987654321","123456"],
+    "CG": ["987654321","123456"],
+    "DT":["987654321","123456"],
+    "WCS":["987654321","123456"]}   
+    return Subs.get(sub,"none")
+
+def getTimeTable(day):
+    # Edit Here
+    # Day(Mon=0) Subject and time
+    D0=['DS','CO','NMPS','OS','DS']
+    D1=['OS','DMS','DS','CG','NMPS']
+    D2=['DMS','CG','DS','CO','DT']
+    D3=['CG','NMPS','CO','DMS','DS']
+    D4=['NMPS','OS','DT','OS','WCS']
+    Days=[D0,D1,D2,D3,D4]
+    return Days[day]   
+
+def getDayOfWeek():
+    now = datetime.datetime.now()
+    return now.weekday()
+
+def getCurrentTime():
+    now=datetime.datetime.now()
+    return now.strftime("%H:%M")
+    
+def joinMe():
+    today=getDayOfWeek()
+    todayTimeTable=getTimeTable(today)
+    # Edit Here
+    # Set Your Timing According to Time Table in HH:MM format(24 Hr)
+    timing={"10:02":0,"11:02":1,"12:02":2,"14:02":3,"15:02":4}
+
+    subIndex=timing.get(getCurrentTime(),7)
+    if(subIndex==7):
+        return 
+    currentSubject=todayTimeTable[subIndex]
+    [meetingID,meetingPassword]= getID(currentSubject)
+
+    # Edit Here
+    # default is 45 Min change total meeting time Accordinly
+    meetingTime=45
+    zoomClass(meetingID,meetingPassword,meetingTime)
+    return 
+
+
+print('Hold (Ctrl+c) to exit the program ')
+while True:
+    today=getDayOfWeek()
+    if(today==5 or today==6):
+        # Edit Here
+        # Sleep for hour if day is Saturday or Sunday
+        time.sleep(3600) 
+    else:
+        joinMe()
+        time.sleep(10)
+
+#Author - SANJAY SINGH     
